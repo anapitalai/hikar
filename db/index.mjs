76b152daaaea6122ed9ce1f7d0a2218a56429cf3
Alois Napitalai <anapitalai@gmail.com>
2023-04-21 07:50:27 +1000

    Dockerise the hikar app

--- db/index.mjs
+++ db/index.mjs
@@ -3,6 +3,7 @@ import pg from 'pg';
 const db = new pg.Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
     user: process.env.DB_USER
 });
 
